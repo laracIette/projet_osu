b894906366b96e29c291225fae43ef05fad27567
Plunne <plunne.celeste@gmail.com>
2022-03-03 03:39:29 +0100

    abonnes toi

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from run import menu
 
+# Abonnez vous a ma chaine YouTube, n'hesitez pas a laisser un pouce bleu et a commenter.
+# Pensez a activer la cloche pour recevoir les notifications de nouvelle video.
+# Merci d'avoir regarde la video, reposez vous bien, a bientot pour une prochaine video.
 pygame.init()
 
 menu()
